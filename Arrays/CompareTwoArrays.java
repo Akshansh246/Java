c367e0ec424ad5b703cff7e1d0ceed1d862267cd
Akshansh246 <akshanshsaraf40@gmail.com>
2025-06-13 23:31:43 +0530

    13/06/2025 Commit

--- Arrays/CompareTwoArrays.java
+++ Arrays/CompareTwoArrays.java
@@ -4,7 +4,7 @@ public class CompareTwoArrays {
         int[] arr2 = {1,2,3,6,5};
         boolean flag = true;
         for (int i = 0; i < arr2.length; i++) {
-            if (arr1[i]==arr2[i]) {
+            if (arr1[i]!=arr2[i]) {
                 flag = false;
                 break;
             }
